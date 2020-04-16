b1cc5856223612ed0b40d168aeb6929bfc9e8b3e
ranjith <ranjith.v@blinkin.io>
2020-04-16 14:54:22 +0530

    updated clipboard and onclick for filename

--- src/lib/Table/TableList.js
+++ src/lib/Table/TableList.js
@@ -1,35 +1,33 @@
 import React from "react";
-import { Button } from "reactstrap";
 
 export default function TableList(props) {
-  let { preview, fileList, bytesToSize, icons } = props;
+  let { preview, fileList, bytesToSize, icons, copyClipBoard } = props;
 
   return fileList.map((file) => {
     let filesize = bytesToSize(file.file_size);
     return (
       <tr key={`${file.upload_name} ${Math.random()}`}>
-        <th scope="row">
-          <i className={icons(file.file_extension)} aria-hidden="true"></i>{" "}
-          {file.upload_name}
+        <th scope="row" className="table_header_filename">
+          <i className={icons(file.file_extension)} aria-hidden="true"></i>
+          <span
+            className="file_name_preview"
+            onClick={() => preview(file, filesize)}
+            title={file.upload_name}
+          >
+            {file.upload_name}
+          </span>
         </th>
         <td>{file.upload_description}</td>
         <td>{file.file_extension}</td>
         <td>{filesize}</td>
         <td>{file.created_at}</td>
         <td>
-          <Button
-            color="primary"
-            className="btn-sm"
-            onClick={() => preview(file, filesize)}
-          >
-            Preview
-          </Button>
+          <i
+            onClick={() => copyClipBoard(file.file_url)}
+            className="fa fa-clone"
+            aria-hidden="true"
+          ></i>
         </td>
-        {/* <td> 
-          <Button color="danger" className="btn-sm">
-            Delete
-          </Button>
-        </td>*/}
       </tr>
     );
   });
