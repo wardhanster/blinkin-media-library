af2cbf5f9c9f9bc2ded72feed69989115edfa010
ranjith <ranjith.v@blinkin.io>
2020-04-24 16:25:08 +0530

    updated webpack with handling all kinds of files

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,18 @@ module.exports = {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
       {
-        test: /\.css$/,
-        loader: "style-loader!css-loader",
+        test: /\.(jpe?g|png|gif)$/,
+        loaders: ["file-loader"],
       },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff",
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader",
+      },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
     ],
   },
   externals: {
